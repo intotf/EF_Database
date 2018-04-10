207981aeecb172a66f489e8e2b12bb8fc7ff3c32
intotf@163.com <intotf@163.com>
2018-04-10 09:24:07 +0800

    测试

---
+++ Test/RabbitConsumers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQServer;
+using KafkaServer;
+
+namespace RabbitConsumers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MqServer = new MqFactory();
+            var i = 0;
+            while (i < 100)
+            {
+                var data = MqServer.Get("测试主题", false);
+                if (data == null)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                else
+                {
+                    i++;
+                    MqServer.Ack(data.Tag);
+                }
+            }
+            Console.WriteLine("RabbitMQ 本次总共处理：{0}条记录", i);
+
+            var kafka = new KafkaMq();
+            //kafka.Send();
+            kafka.Get();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Test/RabbitMQTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RabbitMQServer;
+using System.Text;
+using KafkaServer;
+
+namespace RabbitMQTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var factory = new MqFactory();
+            for (var i = 0; i <= 1000; i++)
+            {
+                factory.Publish("测试主题", Encoding.UTF8.GetBytes("MqContent_" + i.ToString()));
+            }
+        }
+
+        [TestMethod]
+        public void TestKafka()
+        {
+            var opt = new KafkaMq();
+            opt.Send();
+            opt.Get();
+        }
+    }
+}
